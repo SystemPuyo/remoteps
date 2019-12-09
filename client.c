dafa343556daa4ef8f6c7562a82dca1f802bcf04
Hwanyi <37618091+Hwanyi@users.noreply.github.com>
2019-12-09 19:05:41 +0900

    5번 누르면 프로그램 종료

--- client.c
+++ client.c
@@ -14,6 +14,7 @@ void kill_ps(int, char*);
 void detail_ps();
 void hardware_info();
 void get_history();
+void program_exit();
 
 int main(int argc, char * argv[]){
 	struct sockaddr_in servaddr;
@@ -33,24 +34,6 @@ int main(int argc, char * argv[]){
     		exit(0);
   	}
 
-	/*sel = display_menu();
-	switch(sel){
-		case 1:
-		kill_ps();
-		break;
-		case 2:
-		detail_ps();
-		break;
-		case 3:
-		hardware_info();
-		break;
-		case 4:
-		get_history();
-		break;
-		default:
-		fprintf(stderr, "input error\n");
-		exit(1);
-	}*/
   // 에코 서버의 소켓주소 구조체 작성
   	bzero((char * ) & servaddr, sizeof(servaddr));
   	servaddr.sin_family = AF_INET;
@@ -66,6 +49,36 @@ int main(int argc, char * argv[]){
   //연결 되었으면 수신 준비
     puts("서버와 연결됨..");
 
+while(1){
+	sel = display_menu();
+
+	//scanf("%d", &sel);
+	send(s, &sel, sizeof(int), 0);
+	//서버로 입력한 값을 보낸다.
+
+	switch(sel){
+		case 1:
+		//kill_ps();
+		break;
+		case 2:
+		//detail_ps();
+		break;
+		case 3:
+		//hardware_info();
+		break;
+		case 4:
+		//get_history();
+		break;
+		case 5:
+		printf("프로그램을 종료하겠습니다.\n");
+		return 0;
+		default:
+		fprintf(stderr, "input error\n");
+		exit(1);
+	}
+}
+/*
+//여기 밑을 detail_ps로 넘겨도 될 듯
     inet_ntop(AF_INET, & servaddr.sin_addr.s_addr, server_ip, sizeof(server_ip));
     printf("IP : %s ", server_ip);
     printf("Port : %x ", ntohs(servaddr.sin_port));
@@ -81,18 +94,19 @@ int main(int argc, char * argv[]){
     strcat(filename, "_backup");
     fp = open(filename, O_WRONLY | O_CREAT | O_TRUNC);
 
+    printf("file is receiving now.. \n");
     while (total != filesize) {
       sread = recv(s, buf, 100, 0);
-      printf("file is receiving now.. ");
       total += sread;
       buf[sread] = 0;
       write(fp, buf, sread);
       bzero(buf, sizeof(buf));
-      printf("processing : %4.2f\n%%", total * 100 / (float) filesize);
+      printf("processing : %4.2f\r%%", total * 100 / (float) filesize);
       usleep(1000);
 
     }
-
+	printf("\n");
+	//여기까지*/
 }
 
 int display_menu(void) {
@@ -108,6 +122,7 @@ int display_menu(void) {
 		printf("\n\t\t\t=\t2) %s\t\t=", "Show detail ps");
 		printf("\n\t\t\t=\t3) %s\t\t=", "Show hardware info");
 		printf("\n\t\t\t=\t4) %s\t\t=", "Show process use history");
+		printf("\n\t\t\t=\t5) %s\t\t=", "exit");
 		printf("\n\t\t\t=========================================");
 		printf("\n\t\t\t=> ");
 		scanf("%s", input);
@@ -115,7 +130,7 @@ int display_menu(void) {
 		if (strlen(input) != 1) { // some wrong input(not int) ex) abcd, 1abcd, 2!af43 ...
 			continue;
 		}
-		if (menu < 1 || menu > 4) { // int input, but not in menu number
+		if (menu < 1 || menu > 5) { // int input, but not in menu number
 			continue;
 		}
 		else {
